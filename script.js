ae02d9d5a438fe8fb331eef08415bbc07f31c66f
xknRiya <mc4l3xy7@gmail.com>
2025-01-31 20:08:26 -0300

    fixed scrolling function

--- script.js
+++ script.js
@@ -4,33 +4,46 @@ function colorChar(color, char){
   return `<span class="${color}">${char}</span>`
 }
 const colorable = ['<', '>', '/']
-function machineWriting(element, text, time = 300){
+async function machineWriting(element, text, time = 300){
   let i = 0;
-  let l = 0;
   for (const char of text){
-    setTimeout(()=>{
-      element.innerHTML += colorChar(colorable.includes(char) ? 'red' : 'white', char);
-      l++;
-      if (l === text.length){
-        console.log(h2.textContent, l, text.length)
-      }
-    }, time*i)
-    i++;
-
+    
+    await new Promise(resolve =>{
+      setTimeout(()=>{
+        resolve()
+      }, time)
+    })
+    element.innerHTML += colorChar(colorable.includes(char) ? 'red' : 'white', char);
   };
 }
 
-function machineDeleting(element, time = 200){
-  let text = element.textContent
-  text = text.slice(0, text.length)
-  text = text.slice(0, text.length)
-  text = text.slice(0, text.length)
-  text = text.slice(0, text.length)
-  console.log(text.length)
+async function machineDeleting(element, time = 200){
+  let list = []
+
+  for (let char of element.textContent){
+    // el += '>'
+    // if (!el.includes('<span')){
+    //   el += '<' + el;
+    // }
+    // console.log(el); 
+    list.push(colorChar(colorable.includes(char) ? 'red' : 'white', char));
+  }
+  for (let i = list.length; i >= 0; i--){
+    list = list.slice(0, i)
+    await new Promise(resolve => {
+      setTimeout(()=>{
+        resolve();
+      }, time);
+    })
+    element.innerHTML = list.join('')
+  // console.log(list);
+
+  }
 }
 
 const h2 = document.querySelector('h2');
 const dash = document.querySelector('#dash');
+const arrows = document.querySelector('#triangle');
 function loopDash(foo, time = 1000){
   setTimeout(()=>{
     dash.textContent = foo % 2 ? '_' : ' ';
@@ -38,12 +51,29 @@ function loopDash(foo, time = 1000){
   }, time)
 }
 loopDash('', 700)
-machineWriting(h2, '</Welcome>', 100);
-let scrollable = true;
-window.addEventListener('scroll', (event)=>{
-  console.log('999')
-  if(scrollable){
-    machineDeleting(h2, 100);
-    scrollable = false;
+await machineWriting(h2, '</Welcome>', 100);
+let scrollableUp = true;
+let scrollableDown = true;
+
+arrows.style.rotate = '0deg';
+console.log(arrows.style.rotate)
+window.addEventListener('wheel', (event)=>{
+  if(scrollableDown && event.deltaY > 0){
+    scrollableDown = false;
+    scrollableUp = false;
+    machineDeleting(h2, 100)
+      .then(response => {
+        scrollableUp = true;
+        arrows.style.rotate = parseInt(arrows.style.rotate.replace('deg', '')) + 180 + 'deg';
+        console.log(arrows.style.rotate)
+      })
+  } else if (scrollableUp && event.deltaY < 0) {
+    scrollableUp = false;
+    scrollableDown = false;
+    machineWriting(h2, '</Welcome>', 100)
+      .then(response => {
+        scrollableDown = true;
+        arrows.style.rotate = parseInt(arrows.style.rotate.replace('deg', '')) + 180 + 'deg';
+        }) 
   }
-})
+}) 
